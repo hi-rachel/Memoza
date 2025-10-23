8e791b9b81956e562d1f817a6a05311f8d6f00ef
hi-rachel <rachel.uiux@gmail.com>
2025-10-23 18:01:04 +0900

    chore: 카카오 이미지 도메인 추가

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,14 @@ const nextConfig: NextConfig = {
         protocol: "http",
         hostname: "k.kakaocdn.net",
       },
+      {
+        protocol: "https",
+        hostname: "img1.kakaocdn.net",
+      },
+      {
+        protocol: "http",
+        hostname: "img1.kakaocdn.net",
+      },
     ],
   },
 };
